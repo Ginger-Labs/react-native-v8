83b9e627c94c640db2e2bc88aa0d75044a4d7cc0
Mihail Gutan <Mihail@gingerlabs.com>
2019-10-14 14:15:55 -0700

    add space

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,7 @@ rm -rf $BUILD_DIR
 if [ $LOCAL_ONLY = true ] ; then
   echo "Building locally isn't supported yet."
 else
-  if [ $RN_VERSION = true] ; then	
+  if [ $RN_VERSION = true ] ; then	
     echo clonning v: $RN_VERSION into $BUILD_DIR
     git clone --depth=1 --branch ${RN_VERSION} https://github.com/Ginger-Labs/react-native.git $BUILD_DIR
   else 

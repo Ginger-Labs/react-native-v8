0250f715e5a18c8acfe52c8c5ceb0cb7b2c5b32e
Mihail Gutan <Mihail@gingerlabs.com>
2020-04-16 13:10:55 -0700

    Revert "progress"
    This reverts commit 710bce5e8a72110a3b1f31b3afbfcfa39283c2bc.

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,12 +9,11 @@ rm -rf $BUILD_DIR
 if [ $LOCAL_ONLY = true ] ; then
   echo "Building locally isn't supported yet."
 else
-  if [ $RN_PATH ] ; then	
-    echo copying $RN_PATH into $BUILD_DIR
-    #git clone --depth=1 --branch ${RN_VERSION} https://github.com/Ginger-Labs/react-native.git $BUILD_DIR
-    cp -Rf $RN_PATH $BUILD_DIR
+  if [ $RN_VERSION ] ; then	
+    echo clonning v: $RN_VERSION into $BUILD_DIR
+    git clone --depth=1 --branch ${RN_VERSION} https://github.com/Ginger-Labs/react-native.git $BUILD_DIR
   else 
-    echo "RN_PATH is not defined, please export RN_PATH first (the path to .../bagel/node_modules/react-native)"
+    echo "RN_VERSION is not defined, please export RN_VERSION first (the version tag or branch name)"
   fi
 fi
 
@@ -37,10 +36,10 @@ done
 # [1] Build
 ######################################################################################
 
-#cd $BUILD_DIR
-#yarn
-#./gradlew :ReactAndroid:installArchives
+cd $BUILD_DIR
+yarn
+./gradlew :ReactAndroid:installArchives
 
-#mkdir -p $DIST_DIR
-#cd ..
-#cp -Rf $BUILD_DIR/android/* $DIST_DIR
+mkdir -p $DIST_DIR
+cd ..
+cp -Rf $BUILD_DIR/android/* $DIST_DIR
